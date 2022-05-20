913cfc0d6c1f834f79dfb59d0ef476297c4884cc
henriqueRubido <henriquerubido1@gmail.com>
2022-05-20 16:39:55 -0300

    songPage stylization

--- l2l/src/pages/SongPage.tsx
+++ l2l/src/pages/SongPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { SongCard } from "./AddPage";
 import ReactPlayer from "react-player";
@@ -8,18 +9,37 @@ const SongPage: React.FC = () => {
   const getSongs = localStorage.getItem('songs');
   const parseSongs = typeof getSongs === 'string' && JSON.parse(getSongs);
   const findSong: SongCard = parseSongs.find((song: any) => song.token === splitPathname[2]);
-  console.log(findSong.lyrics);
+  const [ notes, setNotes ] = useState<string>('');
   
   return (
-    <div className="flex">
-      <h1 className="ml-5 text-2xl text-white">{ findSong.song } - { findSong.artist }</h1>
-      <ReactPlayer
-          url={ findSong.youtubeUrl }
-          className='react-player'
-          playing
-          width='20%'
-        />
-        <p className="text-white border border-white w-2/6">{ findSong.lyrics }</p>
+    <div className="text-white">
+      <h1 className="ml-5 text-2xl">{ findSong.song } - { findSong.artist }</h1>
+      <div className="flex m-5">
+        <div className="border border-salmon h-2/5 w-2/6">
+        <ReactPlayer
+            url={ findSong.youtubeUrl }
+            className='react-player'
+            playing
+            loop={true}
+            controls={true}
+            width="100%"
+          />
+        </div>
+        <div className="ml-10 bg-black border border-salmon rounded-md p-2">
+          { findSong.lyrics.split('\n').map(line => <p className="text-2xl m-1">{ line }</p>)}
+        </div>
+        <div className="ml-5 w-3/5">
+          <textarea
+            className="p-2 ml-10 w-5/6 h-80 rounded-md bg-black border border-salmon text-xl"
+            cols={10}
+            rows={10}
+            placeholder="take notes!"
+            required
+          />
+          <div className="p-2 ml-10 w-5/6 h-80 rounded-md bg-black border border-salmon text-xl">
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
